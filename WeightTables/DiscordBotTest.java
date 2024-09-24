5ebff325aa4b68eb7e7c0b0cd5217ec237c3445d
Eric Andreae <eandreae@ucsc.edu>
2024-09-23 19:27:15 -0700

    more more bot

--- WeightTables/DiscordBotTest.java
+++ WeightTables/DiscordBotTest.java
@@ -22,7 +22,7 @@ public class DiscordBotTest {
         // Add all your commands on this action instance
         commands.addCommands(
             Commands.slash("say", "Makes the bot say what you tell it to")
-            .addOption(STRING, "content", "What the bot should say", true), // Accepting a user input
+            .addOption(String, "content", "What the bot should say", true), // Accepting a user input
             Commands.slash("leave", "Makes the bot leave the server")
             .setGuildOnly(true) // this doesn't make sense in DMs
             .setDefaultPermissions(DefaultMemberPermissions.DISABLED) // only admins should be able to use this command.
--- WeightTables/SlashCommandListener.java
+++ WeightTables/SlashCommandListener.java
@@ -8,17 +8,19 @@ public class SlashCommandListener extends ListenerAdapter {
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
       switch (event.getName()) {
-        case "say" -> {
+        case "say" : 
           String content = event.getOption("content", OptionMapping::getAsString);
           event.reply(content).queue();
-        };
-        case "leave" -> {
+          break;
+        
+        case "leave" :
           event.reply("I'm leaving the server now!")
             .setEphemeral(true) // this message is only visible to the command user
             .flatMap(m -> event.getGuild().leave()) // append a follow-up action using flatMap
             .queue(); // enqueue both actions to run in sequence (send message -> leave guild)
-        };
-        default -> return;
+            break;
+        
+        default : return;
       }
     }
   }
